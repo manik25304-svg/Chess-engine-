84a44a83db912b83941f41da9d708642779ed0da
Manik Goel <manik.p4049.viiic@gmail.com>
2026-06-20 16:10:13 +0530

    Initial Commit

---
+++ Chess engine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chess engine
+//
+//  Created by Manik Goel on 20/06/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
